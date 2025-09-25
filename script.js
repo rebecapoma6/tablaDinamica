e58d19c65d2e03ad04f50f10db7432deefae00e6
rebeca poma <mrebecapoma.6@gmail.com>
2025-09-25 18:25:41 +0200

    new_changes

--- script.js
+++ script.js
@@ -1,46 +1,94 @@
+// const tableElement = document.createElement("table");
+
+// let columnas
+
+// let nombres = [
+//   "Adriana", "Miguel", "Camila", "Sofía", "Luis", "Valeria", "José", "Marcela", "Ricardo", "Ana",
+//   "Carlos", "Isabel", "María", "Victoria", "Leonardo", "Javier", "Rosa", "Pedro", "Daniela", "Andrés"
+// ];
+
+// const trElement = document.createElement("tr");
+// const maxColumns = 2;
+    
+
+// for (let th = 0; th < maxColumns; th++) {
+//     maxColumns.innerText = "nombre","edad";
+//     const thElement = document.createElement("th");
+//     thElement.innerText = nombres[th];
+//     tableElement.appendChild(thElement)
+// }
+
+// tableElement.appendChild(trElement);
+// document.body.appendChild(tableElement);
+
+// const buttonElement = document.createElement("button");
+// buttonElement.innerText = "Añade otra fila";
+
+// function newRow() {//nuevafila
+//     const trElement = document.createElement("tr");//creamos el elemento fila
+
+//         let randomId = Math.floor(Math.random() * nombres.length);
+//         let randomName = nombres[randomId]
+//         const tdElement = document.createElement("td");
+//         tdElement.innerText = randomName;
+    
+    
+//     trElement.appendChild(tdElement);
+//     tableElement.appendChild(trElement);
+  
+// }
+
+
+// buttonElement.addEventListener("click",newRow);
+// document.body.appendChild(buttonElement)
+
+
+
 const tableElement = document.createElement("table");
-
-let columnas
-
-let nombres = [
-  "Adriana", "Miguel", "Camila", "Sofía", "Luis", "Valeria", "José", "Marcela", "Ricardo", "Ana",
-  "Carlos", "Isabel", "María", "Victoria", "Leonardo", "Javier", "Rosa", "Pedro", "Daniela", "Andrés"
+let columnas = ["Nombre", "Edad"];
+const nombres = [
+  "Alicia", "Bruno", "Carla", "Diego", "Elena",
+  "Fernando", "Gabriela", "Hugo", "Isabel", "Javier",
+  "Karina", "Luis", "Marina", "Nicolás", "Olga",
+  "Pablo", "Raquel", "Sergio", "Teresa", "Víctor"
 ];
 
 const trElement = document.createElement("tr");
 const maxColumns = 2;
-    
-
-for (let th = 0; th < maxColumns; th++) {
-    maxColumns.innerText = "nombre","edad";
-    const thElement = document.createElement("th");
-    thElement.innerText = nombres[th];
-    tableElement.appendChild(thElement)
+for(let th = 0; th < maxColumns; th++){
+  // Creamos cada una de las columnas
+  const thElement = document.createElement("th");
+  // Le damos un nombre a cada columna
+  thElement.innerText = columnas[th];
+  // Añadimos la columna ya formada a la fila
+  trElement.appendChild(thElement);
 }
-
+// Añadimos a la tabla la fila ya formada
 tableElement.appendChild(trElement);
+// Añadimos al body la tabla ya formada
 document.body.appendChild(tableElement);
 
 const buttonElement = document.createElement("button");
-buttonElement.innerText = "Añade otra fila";
+buttonElement.innerText = "Añadir fila";
 
-function newRow() {//nuevafila
-    const trElement = document.createElement("tr");//creamos el elemento fila
-
-        let randomId = Math.floor(Math.random() * nombres.length);
-        let randomName = nombres[randomId]
-        const tdElement = document.createElement("td");
-        tdElement.innerText = randomName;
-    
-    
-    trElement.appendChild(tdElement);
-    tableElement.appendChild(trElement);
-  
+function newRow(){
+  // Creamos el elemento fila
+  const trElement = document.createElement("tr");
+  // Creamos un td para el nombre
+  let randomId = Math.floor(Math.random() * nombres.length);
+  let randomName = nombres[randomId];
+  let tdElement = document.createElement("td");
+  tdElement.innerText = randomName;
+  trElement.appendChild(tdElement);
+  // Creamos un td para la edad
+  let randomAge = Math.floor(Math.random() * 100);
+  tdElement = document.createElement("td");
+  tdElement.innerText = randomAge;
+  trElement.appendChild(tdElement);
+  // Añadimos la fila ya formada a la tabla
+  tableElement.appendChild(trElement);
 }
 
+buttonElement.addEventListener("click", newRow);
 
-buttonElement.addEventListener("click",newRow);
-document.body.appendChild(buttonElement)
-
-
-
+document.body.appendChild(buttonElement);
